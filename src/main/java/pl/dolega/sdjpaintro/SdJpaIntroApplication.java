6df100107f448adab5e509cce6f171b3b5c3e900
marcin <marcin_dolega@epam.com>
2022-07-12 12:22:36 +0200

    Initial commit

---
+++ src/main/java/pl/dolega/sdjpaintro/SdJpaIntroApplication.java
@@ -0,0 +1,13 @@
+package pl.dolega.sdjpaintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdJpaIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdJpaIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dolega/sdjpaintro/SdJpaIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dolega.sdjpaintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdJpaIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
