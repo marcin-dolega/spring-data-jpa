7e9b14d047edf7a1090f28c93f62bc6cad133f41
marcin <marcin_dolega@epam.com>
2022-07-13 16:23:57 +0200

    6. Spring Boot Test

--- src/test/java/pl/dolega/sdjpaintro/SdJpaIntroApplicationTests.java
+++ src/test/java/pl/dolega/sdjpaintro/SdJpaIntroApplicationTests.java
@@ -1,10 +1,23 @@
 package pl.dolega.sdjpaintro;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.dolega.sdjpaintro.repositories.BookRepository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SdJpaIntroApplicationTests {
+
+	@Autowired
+	BookRepository bookRepository;
+
+	@Test
+	void testBookRepository() {
+		long count = bookRepository.count();
+		assertThat(count).isPositive();
+	}
 
 	@Test
 	void contextLoads() {
