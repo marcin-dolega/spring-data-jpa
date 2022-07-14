760743cf5432be63aaee0abc384ebb2b8056e6de
marcin <marcin_dolega@epam.com>
2022-07-14 10:59:18 +0200

    10. MySQL Spring Boot Configuration

--- src/main/java/pl/dolega/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/pl/dolega/sdjpaintro/bootstrap/DataInitializer.java
@@ -16,13 +16,14 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
         Book bookDDD = new Book("Domain Driven Design", "0321125215", "Addison-Wesley Professional");
+        Book bookSIA = new Book("Spring in Action", "9781617297571", "O'Reilly");
+
         System.out.println("Id: " + bookDDD.getId());
         Book savedDDD = bookRepository.save(bookDDD);
+
         System.out.println("Id: " + bookDDD.getId());
-
-
-        Book bookSIA = new Book("Spring in Action", "9781617297571", "O'Reilly");
         Book savedSIA = bookRepository.save(bookSIA);
 
         bookRepository.findAll()
