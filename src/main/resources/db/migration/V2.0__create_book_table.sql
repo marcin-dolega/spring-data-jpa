270f8a73730a632eec6c7c24616327191655994e
marcin <marcin_dolega@epam.com>
2022-07-19 16:38:17 +0200

    19. Clean and Rebuild with Flyway

--- src/main/resources/db/migration/V2.0__create_book_table.sql
+++ src/main/resources/db/migration/V2.0__create_book_table.sql
@@ -1,7 +1,7 @@
 drop table if exists book;
 
 create table book (
-    id bigint not null,
+    id bigint not null AUTO_INCREMENT,
     isbn varchar(255),
     publisher varchar(255),
     title varchar(255),
--- src/main/resources/db/migration/V3.0__create_author_table.sql
+++ src/main/resources/db/migration/V3.0__create_author_table.sql
@@ -1,7 +1,7 @@
 drop table if exists author;
 
 create table author (
-    id bigint not null,
+    id bigint not null AUTO_INCREMENT,
     first_name varchar(255),
     last_name varchar(255),
     primary key (id)
