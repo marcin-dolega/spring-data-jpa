3934866f9dba8073fefce4a497cb8fcc5559d997
marcin <marcin_dolega@epam.com>
2022-07-20 15:20:09 +0200

    25. Unit Tests for UUID Primary Keys

--- src/test/java/pl/dolega/sdjpaintro/MySQLIntegrationTest.java
+++ src/test/java/pl/dolega/sdjpaintro/MySQLIntegrationTest.java
@@ -6,9 +6,15 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
+import pl.dolega.sdjpaintro.domain.AuthorUuid;
+import pl.dolega.sdjpaintro.domain.BookUuid;
+import pl.dolega.sdjpaintro.repositories.AuthorUuidRepository;
 import pl.dolega.sdjpaintro.repositories.BookRepository;
+import pl.dolega.sdjpaintro.repositories.BookUuidRepository;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ActiveProfiles("local")
 @DataJpaTest
@@ -19,9 +25,33 @@ public class MySQLIntegrationTest {
     @Autowired
     BookRepository bookRepository;
 
+    @Autowired
+    AuthorUuidRepository authorUuidRepository;
+
+    @Autowired
+    BookUuidRepository bookUuidRepository;
+
     @Test
     void testMySQL() {
         long countBefore = bookRepository.count();
         assertThat(countBefore).isEqualTo(2);
+    }
+
+    @Test
+    void testAuthorUuid() {
+        AuthorUuid expected = new AuthorUuid("Martyn", "Groch");
+        authorUuidRepository.save(expected);
+        var actual = authorUuidRepository.getReferenceById(expected.getId());
+        assertNotNull(actual);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testBookUuid() {
+        BookUuid expected = new BookUuid("Przygody Groszka", "666", "Wilkowisko");
+        bookUuidRepository.save(expected);
+        var actual = bookUuidRepository.getReferenceById(expected.getId());
+        assertNotNull(actual);
+        assertEquals(expected, actual);
     }
 }
