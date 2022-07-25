d413a29be13985dd6714c7346ec5b0eec3c69610
marcin <marcin_dolega@epam.com>
2022-07-25 14:54:17 +0200

    6. Implement Get Author By Id

--- src/main/java/pl/dolega/sdjpaintro/jdbc/author/AuthorDaoImpl.java
+++ src/main/java/pl/dolega/sdjpaintro/jdbc/author/AuthorDaoImpl.java
@@ -1,9 +1,38 @@
 package pl.dolega.sdjpaintro.jdbc.author;
 
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@Component
 public class AuthorDaoImpl implements AuthorDao {
+
+    private final DataSource source;
+
+    public AuthorDaoImpl(DataSource source) {
+        this.source = source;
+    }
 
     @Override
     public Author getById(Long id) {
+        try {
+            Connection connection = source.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM author WHERE id = " + id);
+            if (resultSet.next()) {
+                Author author = new Author();
+                author.setId(id);
+                author.setFirstName(resultSet.getString("first_name"));
+                author.setLastName(resultSet.getString("last_name"));
+                return author;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
--- src/test/java/pl/dolega/sdjpaintro/jdbc/AuthorDaoIntegrationTest.java
+++ src/test/java/pl/dolega/sdjpaintro/jdbc/AuthorDaoIntegrationTest.java
@@ -1,18 +1,17 @@
 package pl.dolega.sdjpaintro.jdbc;
 
-import guru.springframework.jdbc.dao.AuthorDao;
-import guru.springframework.jdbc.dao.AuthorDaoImpl;
-import guru.springframework.jdbc.domain.Author;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import pl.dolega.sdjpaintro.jdbc.author.Author;
+import pl.dolega.sdjpaintro.jdbc.author.AuthorDao;
+import pl.dolega.sdjpaintro.jdbc.author.AuthorDaoImpl;
 
-/**
- * Created by jt on 8/20/21.
- */
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @ActiveProfiles("local")
 @DataJpaTest
 @Import(AuthorDaoImpl.class)
