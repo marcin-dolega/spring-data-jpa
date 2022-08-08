ed54b2fbbea0c666da8c607f406a49234b31edda
marcin <marcin_dolega@epam.com>
2022-08-08 16:50:06 +0200

    7. Implement Delete Author

--- src/test/java/pl/dolega/sdjpaintro/jdbc/AuthorDaoIntegrationTest.java
+++ src/test/java/pl/dolega/sdjpaintro/jdbc/AuthorDaoIntegrationTest.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 import pl.dolega.sdjpaintro.author.Author;
 import pl.dolega.sdjpaintro.author.AuthorDao;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ActiveProfiles("local")
 @DataJpaTest
@@ -57,7 +59,7 @@ class AuthorDaoIntegrationTest {
         Author author = new Author("John", "Thompson");
         Author saved = authorDao.saveNewAuthor(author);
         authorDao.deleteAuthorById(saved.getId());
-        Author deleted = authorDao.getById(saved.getId());
-        assertThat(deleted).isNull();
+        assertThrows(EmptyResultDataAccessException.class, () -> authorDao.getById(saved.getId()));
+//        assertThat(deleted).isNull();
     }
 }
