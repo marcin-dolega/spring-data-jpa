22379d89d0be412dacc5aef872a228687101e515
Marcin Dolega <marcin92d@gmail.com>
2022-09-15 16:54:35 +0200

    5. Null Handling

--- src/main/java/pl/dolega/sdjpaintro/book/BookRepo.java
+++ src/main/java/pl/dolega/sdjpaintro/book/BookRepo.java
@@ -1,9 +1,13 @@
 package pl.dolega.sdjpaintro.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 
 import java.util.Optional;
 
 public interface BookRepo extends JpaRepository<Book, Long> {
     Optional<Book> findBookByTitle(String title);
+    Book readByTitle(String title);
+    @Nullable
+    Book getByTitle(@Nullable String title);
 }
--- src/test/java/pl/dolega/sdjpaintro/BookRepositoryTest.java
+++ src/test/java/pl/dolega/sdjpaintro/BookRepositoryTest.java
@@ -1,11 +1,41 @@
 package pl.dolega.sdjpaintro;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
+import pl.dolega.sdjpaintro.book.Book;
+import pl.dolega.sdjpaintro.book.BookRepo;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ActiveProfiles("local")
 @DataJpaTest
-@ComponentScan
+@ComponentScan(basePackages = {"pl.dolega.sdjpaintro.book", "pl.dolega.sdjpaintro.author"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BookRepositoryTest {
+
+    @Autowired
+    BookRepo bookRepo;
+
+    @Test
+    void testEmptyResultException() {
+        assertThrows(EmptyResultDataAccessException.class, () -> {
+            Book book = bookRepo.readByTitle("Szajba");
+        });
+    }
+
+    @Test
+    void testNullParam() {
+        assertNull(bookRepo.getByTitle(null));
+    }
+
+    @Test
+    void testNoException() {
+        assertNull(bookRepo.getByTitle("Groch"));
+    }
 }
